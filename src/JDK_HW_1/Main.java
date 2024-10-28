9b33840ed29b95badfb14b486d1ed1e78ff8782b
Svetlana00713 <123585447+Svetlana00713@users.noreply.github.com>
2024-10-29 02:07:47 +0300

    Added README

--- src/JDK_HW_1/Main.java
+++ src/JDK_HW_1/Main.java
@@ -3,10 +3,6 @@ package JDK_HW_1;
 public class Main {
     public static void main(String[] args) {
 
-        int distanceBetweenWindows = 100;
-        int screenWidth = 800;
-        int screenHeight = 600;
-
         Server server = new Server();
         new Client(server);
         new Client(server);
